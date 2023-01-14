c212a0bb485e28ce42c93e43e91fba46eb4f5cce
meniYud <menyud@gmail.com>
2023-01-14 17:58:20 +0200

    turn on persistent mode

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,22 @@ import Menu from './Menu.js';
 import Order from './Order.js';
 import Router from './Router.js';
 
+// Request persistance storage
+(async function() {
+    // note that navigator.storage is not available in un-secured context.
+    // so when running locally, run with localhost:3000 instead of X.X.X.X:3000
+    if (navigator.storage && navigator.storage.persist) {
+        const isAlreadyPersistent = await navigator.storage.persisted();
+        if (!isAlreadyPersistent) {
+            const result = await navigator.storage.persist();
+            console.log(`Was Persistent Storage Request granted? ${result}`);
+        } else {
+            console.log(`Persistent Storage already granted`)
+        }
+    }
+  }
+)();
+
 window.addEventListener("DOMContentLoaded", () => {
     Router.init();
     Menu.load();
